0e361562fdf96b2e09c0db61d6726ca0b9426a00
Pratiksha Gupta <80025856+Pratiksha102@users.noreply.github.com>
2022-04-10 00:24:34 +0530

    Hooks in React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-
+import TextForm from './components/TextForm';
 
 let name="Pratiksha"
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <h1>Hello {name}</h1>
-    <div className="container">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem iure explicabo tenetur? Harum distinctio et qui earum deserunt quibusdam, aperiam aut perspiciatis repudiandae maiores minus laudantium ab necessitatibus incidunt repellendus?
-    </div>
+
     <Navbar title="Pratiksha"></Navbar>
+    <div className="container">
+    <TextForm heading="Enter the text to analyze"></TextForm>
+    </div>
+    
     </>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,33 @@
+import React,{useState} from 'react'
+
+export default function TextForm(props) {
+
+    const handleOnClick=()=>{
+        
+        console.log("on click function is clicked");
+        let newText=text.toUpperCase();
+        setText(newText);
+
+    }
+
+    const handleOnChange=(event)=>{
+
+        setText(event.target.value);
+
+
+    }
+    const [text,setText]=useState('Enter new text here');
+    
+  return (
+      
+    <form>
+        <h3 className="my-3">{props.heading}</h3>
+        <div className="form-group">
+    <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
+    <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+  
+  <button  className="btn btn-primary" onClick={handleOnClick}>Change it to UpperCase</button>
+</form>
+  )
+}
